50287cb681a021969631253c4b8f34e647ea0736
何树尧 <hsy_wy_email@163.com>
2021-08-26 12:11:12 +0800

    update

--- src/main/java/lambda/CustomerTest.java
+++ src/main/java/lambda/CustomerTest.java
@@ -1,9 +1,14 @@
 package lambda;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.util.ObjectUtil;
 import lambda.entity.Person;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -18,15 +23,25 @@ import java.util.function.Consumer;
 public class CustomerTest {
 
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setUsername("hsy").setAge(11);
-        //可以添加person的额外字段
-        Map<String, Object> objectMap = toMap(person, map -> map.put("sex", "man"));
-        objectMap.forEach((k, v)->{
-            System.out.println(k+"="+v);
-        });
+        // Person person = new Person();
+        // person.setUsername("hsy").setAge(11);
+        // //可以添加person的额外字段
+        // Map<String, Object> objectMap = toMap(person, map -> map.put("sex", "man"));
+        // objectMap.forEach((k, v)->{
+        //     System.out.println(k+"="+v);
+        // });
         //也可以不添加
         // toMap(person, null);
+
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(null);
+        list2.add(null);list2.add(null);list2.add(null);
+        Collections.copy(list2, list1);
+        list2.forEach(System.out::println);
     }
 
 
